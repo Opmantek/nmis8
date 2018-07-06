2057e37bc80984684f59e8cdf3ef7490516cd57a
Alexander Zangerl <alexz@opmantek.com>
2018-07-06 11:20:03 +1000

    nmis8, SUPPORT-4763, handle weird icmp echo response packet duplication better, correct some bugs related to fallback polling
    fpingd.pl: now catches and complains about fping responses ala
    weirdnode : xmt/rcv/%return = 2/64/3200%, min/avg/max = 0.49/7.39/8.13
    ie. 32x the expected response packets. loss is set to zero in that case.
    
    fpingd.pl: handlee unparseable fping responses better. nmis-fping.json
    state for such nodes now contains 'pending' marker and no
    avg min max loss lastping (which were from past runs, hence not correct anymore)
    
    nmis.pl: runping() now handles multi-homed nodes more correctly
    and survives node configs with host_backup present but blank (which the
    gui can cause). now also handles the case of fpingd having reported no data a/v
    for a node via nonexistent lastping.

--- bin/nmis.pl
+++ bin/nmis.pl
@@ -805,7 +805,7 @@ sub	runThreads
 		$D->{collect}{option} = 'gauge,0:U';
 		$D->{total}{value} = Time::HiRes::time() - $starttime;
 		$D->{total}{option} = 'gauge,0:U';
-		
+
 		# OMK-4630 add nodecount to be saved, probably the following will work well
 		#$D->{nodecount}{value} = $nodecount;
 		#$D->{nodecount}{option} = 'gauge,0:U';
@@ -1541,12 +1541,14 @@ sub runPing
 				 && ref($PT) eq "HASH")
 		{
 			# for multihomed nodes there are two records to check, keyed nodename:N
-			my @tocheck = (defined $NC->{node}->{host_backup}?
+			my @tocheck = ((defined($NC->{node}->{host_backup})
+											&& $NC->{node}->{host_backup})?
 										 grep($_ =~ /^$nodename:\d$/, keys %$PT)
 										 : $nodename);
 			for my $onekey (@tocheck)
 			{
-				if (ref($PT->{$onekey}) eq "HASH"
+				if (ref($PT->{$onekey}) eq "HASH" # present
+						&& defined($PT->{$onekey}->{lastping}) # and valid
 						&& (time - $PT->{$onekey}->{lastping}) < $staleafter)	 # and not stale, 15 minutes seems ample
 				{
 					# copy the fastping data...
