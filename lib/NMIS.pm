b99e4063816155e0c431016f45ec2bcfbc0d2bd5
Alexander Zangerl <alexz@opmantek.com>
2018-10-11 11:42:01 +1000

    nmis8, version revved to 8.6.8a to indicate no longer close to 8.6.7 state

--- lib/NMIS.pm
+++ lib/NMIS.pm
@@ -27,7 +27,7 @@
 #
 # *****************************************************************************
 package NMIS;
-our $VERSION = "8.6.7G";
+our $VERSION = "8.6.8a";
 
 use NMIS::uselib;
 use lib "$NMIS::uselib::rrdtool_lib";
