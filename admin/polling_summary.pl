157bfd4b1e5395b29914baaefdbabf4a7f8126a9
Keith Sinclair <keiths@opmantek.com>
2020-03-20 17:03:46 +1000

    Added specific info for snmpdown pingdown pingonly

--- admin/polling_summary.pl
+++ admin/polling_summary.pl
@@ -39,12 +39,12 @@ use strict;
 use func;
 use NMIS;
 use rrdfunc;
+use Data::Dumper;
 
 my %arg = getArguements(@ARGV);
 
 # Set debugging level.
 my $debug = setDebug($arg{debug});
-$debug = 1;
 
 my $C = loadConfTable(conf=>$arg{conf},debug=>$arg{debug});
 
@@ -52,6 +52,8 @@ my $LNT = loadLocalNodeTable();
 
 my $totalPoll = 0;
 my $goodPoll = 0;
+my $pingDown = 0;
+my $snmpDown = 0;
 my $noSnmp = 0;
 my $demoted = 0;
 my $latePoll5m = 0;
@@ -63,8 +65,8 @@ my @polltimes;
 my $PP = loadTable(dir=>'conf',name=>"Polling-Policy");
 
 # define the output heading and the print format
-my @heading = ("node", "status", "nodestatus", "policy", "delta", "snmp", "poll", "update", "pollmessage");
-printf "%-16s %-8s %-12s %-10s %-6s %-4s %-6s %-7s %-16s\n", @heading;
+my @heading = ("node", "status", "ping", "snmp", "policy", "delta", "snmp", "poll", "update", "pollmessage");
+printf "%-16s %-9s %-5s %-5s %-10s %-6s %-4s %-6s %-7s %-16s\n", @heading;
 
 foreach my $node (sort keys %{$LNT}) {
 	if ( getbool($LNT->{$node}{active}) ) {
@@ -99,19 +101,34 @@ foreach my $node (sort keys %{$LNT}) {
 			$delta = "---";
 		}
 		
+#$VAR1 = {
+#  'failover_ping_status' => undef,
+#  'failover_status' => undef,
+#  'overall' => -1,
+#  'ping_enabled' => 1,
+#  'ping_status' => 1,
+#  'primary_ping_status' => undef,
+#  'snmp_enabled' => 1,
+#  'snmp_status' => 1,
+#  'wmi_enabled' => 0,
+#  'wmi_status' => undef
+#};
 
 		my %status = PreciseNodeStatus(system => $S);
+		print Dumper \%status if $debug > 5;
 
-		my $nodestatus = "reachable";
-		if ( not $status{'overall'} ) { $nodestatus = "unreachable" }
-		elsif ( $status{'overall'} == -1 ) { $nodestatus = "degraded" }
+		my $ping_status = $status{'ping_status'} ? "up" : "down";
+		++$pingDown if $ping_status eq "down";
+
+		my $snmp_status = $status{'snmp_status'} ? "up" : "down";
+		++$snmpDown if $snmp_status eq "down";
 
 		if ( $LNT->{$node}{collect} eq "false" 
 			or  $LNT->{$node}{collect_snmp} eq "false" 
 
 		) {
 			$message = "no snmp collect";
-			$pollstatus = "no_snmp";
+			$pollstatus = "pingonly";
 			++$noSnmp;
 		}
 		elsif ( defined $NI->{system}->{demote_grace} and $NI->{system}->{demote_grace} > 0 ) {
@@ -152,9 +169,9 @@ foreach my $node (sort keys %{$LNT}) {
 			$polltime = $stats->{polltime}->{mean};
 			$updatetime = $stats->{updatetime}->{mean};			
 		}
-		printf "%-16s %-8s %-12s %-10s %-6s %-4s %-6s %-7s %-16s\n", $node, $pollstatus, $nodestatus, $polling_policy, $delta, $snmp, $polltime, $updatetime, $message;
+		printf "%-16s %-9s %-5s %-5s %-10s %-6s %-4s %-6s %-7s %-16s\n", $node, $pollstatus, $ping_status, $snmp_status, $polling_policy, $delta, $snmp, $polltime, $updatetime, $message;
 	}
 }
 
-print "totalPoll=$totalPoll ontime=$goodPoll no_snmp=$noSnmp demoted=$demoted 1x_late=$latePoll5m 3x_late=$latePoll15m 12x_late=$latePoll1h 144x_late=$latePoll12h\n";
+print "totalPoll=$totalPoll ontime=$goodPoll pingDown=$pingDown snmpDown=$snmpDown pingonly=$noSnmp demoted=$demoted 1x_late=$latePoll5m 3x_late=$latePoll15m 12x_late=$latePoll1h 144x_late=$latePoll12h\n";
 
